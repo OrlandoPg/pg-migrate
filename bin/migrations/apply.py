22d3929c5517b8cdd2f309b23a43b4078eb99389
David Rogers <david@ethos-development.com>
2011-11-22 17:23:30 -0500

    I needed some branching logic within "apply" that I really didn't want to implement in Python, so I made a "migrate()" stored procedure in the "migrations" schema to handle that logic for me. This simplified my "apply" command significantly... And taught me a lot about PL/PgSQL. O_O

--- bin/migrations/apply.py
+++ bin/migrations/apply.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python2.7
 
-import base
+import base, subprocess
 
 class MigrationApply(base.MigrationCommand):
     def init_args ( self ):
         super(MigrationApply, self).init_args(
+        ).with_schema_file(
         ).with_migration_path(
         ).with_psql_arguments(
         ).add_argument('MIGRATION_NAME',
@@ -18,8 +19,31 @@ class MigrationApply(base.MigrationCommand):
         )
 
 
-    def main ( self, migration_path, DBNAME, DBUSER, direction, **kwargs ):
-        print migration_path, DBNAME, DBUSER, direction
+    def up_from ( self, DBNAME, DBUSER, hash ):
+        subprocess.call(('psql', DBNAME, DBUSER, '-1c',
+            '''SELECT 1 FROM "migrations"."migrate" ('%(hash)s', 'UP');''' % locals()
+        ))
+
+        # TODO: Make dumping the schema to file automatic...?
+        #subprocess.call(('pg_dump', DBNAME, '-xOsn public') + (('--username', DBUSER) if DBUSER else ( )))
+
+
+    def down_to ( self, DBNAME, DBUSER, hash ):
+        subprocess.call(('psql', DBNAME, DBUSER, '-1c',
+            '''SELECT 1 FROM "migrations"."migrate" ('%(hash)', 'DOWN');''' % locals()
+        ))
+
+
+    def main ( self, schema_file, migration_path, DBNAME, DBUSER, MIGRATION_NAME, direction, **kwargs ):
+        schema_file, migration_path = map(base.path, (schema_file, migration_path))
+
+        for search in (migration_path, 'migration_', '.pg.sql'):
+            MIGRATION_NAME = MIGRATION_NAME.replace(search, '')
+
+        args = (DBNAME, DBUSER, MIGRATION_NAME)
+
+        self.up_from(*args) if direction else self.down_to(*args)
+
 
 if __name__ == '__main__':
         MigrationApply()()
--- bin/migrations/init.py
+++ bin/migrations/init.py
@@ -20,6 +20,52 @@ CREATE INDEX "history_migration_id_idx" ON "migrations"."history" ( "migration_i
 CREATE INDEX "history_next_migration_id_idx" ON "migrations"."history" ( "next_migration_id" DESC NULLS FIRST );
 
 CREATE INDEX "history_applied_idx" ON "migrations"."history" ( "applied" );
+
+CREATE OR REPLACE FUNCTION "migrations"."migrate" ( _hash varchar, varchar ) RETURNS VOID AS
+$BODY$
+DECLARE
+    _HISTORY_ varchar := '"migrations"."history"';
+
+    _direction varchar := UPPER($2);
+    _migration varchar := '"migrations"."migration_' || _hash || '"';
+
+    _migration_id int; _old_tail_id int; _new_tail_id int;
+BEGIN
+    -- Always execute the "_migration" in the specified "_direction"...
+    EXECUTE 'SELECT 1 FROM ' || _migration || ' ( $1 )' USING _direction;
+
+    -- Determine if the "_hash" has already been registered...
+    EXECUTE 'SELECT "migration_id" FROM ' || _HISTORY_ || ' WHERE "hash" = $1 FOR UPDATE'
+        INTO _migration_id USING _hash;
+
+    CASE
+        WHEN _migration_id IS NOT NULL THEN
+            EXECUTE 'UPDATE ' || _HISTORY_ || ' SET "applied" = $2 ' ||
+                'WHERE "migration_id" = $1'
+            USING _migration_id, CASE
+                WHEN _direction = 'UP' THEN true
+                WHEN _direction = 'DOWN' THEN false
+            END;
+
+        WHEN _migration_id IS NULL AND _direction = 'UP' THEN
+            -- TODO: Move into INSERT trigger on "migrations"."history"...?
+            EXECUTE 'SELECT "migration_id" FROM ' || _HISTORY_ ||
+                ' WHERE "next_migration_id" IS NULL FOR UPDATE'
+            INTO _old_tail_id;
+
+            EXECUTE 'INSERT INTO ' || _HISTORY_ || ' ( "hash", "applied" ) ' ||
+                'VALUES ( $1, true ) RETURNING "migration_id"'
+            INTO _new_tail_id
+            USING _hash;
+
+            EXECUTE 'UPDATE ' || _HISTORY_ || ' SET "next_migration_id" = $1 ' ||
+                'WHERE "migration_id" = $2'
+            USING _new_tail_id, _old_tail_id;
+    END CASE;
+END;
+$BODY$
+LANGUAGE plpgsql;
+
 """,
     'DOWN' : 'DROP SCHEMA "migrations" CASCADE;'
 }
