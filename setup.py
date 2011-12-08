3e01215ac98e42fc4a2391a87c58d5af2c2ee6ee
David Rogers <david@ethos-development.com>
2011-12-08 14:22:43 -0500

    Python's "distutils" requires a "setup.py" to operate.

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python2.7
+
+from distutils.core import setup
+
+setup(name='PG Migrate', version='0.1',
+    description='DB Migration Assistant for PostgreSQL',
+    author='David Rogers', author_email='david@orlandopy.org',
+    url='https://github.com/OrlandoPg/pg-migrate/',
+    packages=['migrations'],
+    scripts=['bin/migrations'],
+)
